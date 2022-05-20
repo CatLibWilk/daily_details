3c516f81fd7cd180248e396a71eb0d271d463ed0
Andrew Wilk <andrewwilk@pennmedicine.upenn.edu>
2022-05-20 06:39:35 -0400

    lined up input and monthly expense doughnut chart

--- src/components/MonthlyExpenseRow.jsx
+++ src/components/MonthlyExpenseRow.jsx
@@ -7,7 +7,7 @@ function MonthlyExpenseRow(props){
         <h3>{props.title.toUpperCase()}</h3>
         <div className="">
 
-          <div className="d-inline-block">
+          <div className="mt-5 d-inline-block">
             <Input cf={ props.update_monthly_expenditures } category={ props.title }/>
           </div>
           <div className="ms-5 d-inline-block">

7239f6c4a7b5ea86e82ceb79e2c5fe6a74b3d214
Andrew Wilk <andrewwilk@pennmedicine.upenn.edu>
2022-05-31 06:58:47 -0400

    removed firebase config and created template file

---
+++ src/components/Firebase/config_template.js
@@ -0,0 +1,16 @@
+import 'firebase/compat/database';
+
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId:"",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  
+  };
+
+
+export default config;
